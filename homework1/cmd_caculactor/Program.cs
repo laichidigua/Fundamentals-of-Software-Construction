8de5cae51ce6acc2154bc54f17d2612b2c7ee02d
laichidigua <912556728@qq.com>
2022-02-25 01:08:09 +0800

    homework

---
+++ homework1/cmd_caculactor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd_caculactor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            double num1=0.0, num2=0.0, num3=0.0;
+            char x;
+            Console.WriteLine("请输入第一个数");
+            num1 =double.Parse( Console.ReadLine());
+            Console.WriteLine("请输入第二个数");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入操作符：+；-；*；/");
+            x = char.Parse(Console.ReadLine());
+            switch (x) {
+                case '+':num3 = num1 + num2;break;
+                case '-': num3 = num1 - num2; break;
+                case '*': num3 = num1 * num2; break;
+                case '/': if (num2 != 0.0) { num3 = num1 / num2; } else { Console.WriteLine("除数不能为0");goto LABLE1; }; break;
+                default: Console.WriteLine("请输入正确的操作符");break;
+            }
+            Console.WriteLine("结果是："+num3);
+        LABLE1:
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework1/homework1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1
+{
+    public partial class Form1 : Form
+    {
+        double num1, num2, num3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            num1 = double.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            num2 = double.Parse(textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num3 = num1 -num2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num3 = num1 * num2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (num2 != 0.0)
+            {
+                num3 = num1 / num2;
+            }
+            else { textBox3.Text = "除数不能为0"; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num3 = num1 + num2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "" + num3;
+        }
+    }
+}
