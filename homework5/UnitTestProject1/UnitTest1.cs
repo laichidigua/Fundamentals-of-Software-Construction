84be34fc34d755d34e2e2d7d16a067c4b85977e7
laichidigua <912556728@qq.com>
2022-03-31 18:05:43 +0800

    增加了一些单元测试

--- homework5/UnitTestProject1/UnitTest1.cs
+++ homework5/UnitTestProject1/UnitTest1.cs
@@ -67,6 +67,170 @@ namespace UnitTestProject1
             ords.Add(order3);
             CollectionAssert.Equals(OrderService.orders,ords);
         }
+        //add_order测试
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Client client1 = new Client("aaa", 111111);
+           
 
+            Order order1 = new Order(client1);
+
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+
+            OrderService.add_order(order1);
+            List<Order> ords = new List<Order>();
+            ords.Add(order1);
+            CollectionAssert.Equals(OrderService.orders, ords);
+        }
+        //delete_order测试
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Client client1 = new Client("aaa", 111111);
+            Client client2 = new Client("bbb", 222222);
+
+            Order order1 = new Order(client1);
+            Order order2 = new Order(client2);
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+            order2.add(Goods.goods4);
+            order2.add(Goods.goods5);
+            OrderService.add_order(order1);
+            OrderService.add_order(order2);
+            OrderService.delete_order(order2);
+            List<Order> ords = new List<Order>();
+            ords.Add(order1);
+           
+            CollectionAssert.Equals(OrderService.orders, ords);
+        }
+        //search_by_number测试
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Client client1 = new Client("aaa", 111111);
+            Client client2 = new Client("bbb", 222222);
+            Client client3 = new Client("ccc", 333333);
+
+            Order order1 = new Order(client1);
+            Order order2 = new Order(client2);
+            Order order3 = new Order(client3);
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+            order2.add(Goods.goods4);
+            order2.add(Goods.goods5);
+            order3.add(Goods.goods3, 2);
+            order3.add(Goods.goods4, 3);
+            order3.add(Goods.goods5);
+            OrderService.add_order(order1);
+            OrderService.add_order(order2);
+            OrderService.add_order(order3);
+
+            Assert.AreEqual(OrderService.search_by_number(1),order1);
+        }
+        //search_by_client测试
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Client client1 = new Client("aaa", 111111);
+            Client client2 = new Client("bbb", 222222);
+            Client client3 = new Client("ccc", 333333);
+
+            Order order1 = new Order(client1);
+            Order order2 = new Order(client2);
+            Order order3 = new Order(client3);
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+            order2.add(Goods.goods4);
+            order2.add(Goods.goods5);
+            order3.add(Goods.goods3, 2);
+            order3.add(Goods.goods4, 3);
+            order3.add(Goods.goods5);
+            OrderService.add_order(order1);
+            OrderService.add_order(order2);
+            OrderService.add_order(order3);
+
+            Assert.AreEqual(OrderService.search_by_client("aaa"), order1);
+        }
+        //search_by_goods测试
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Client client1 = new Client("aaa", 111111);
+            Client client2 = new Client("bbb", 222222);
+            Client client3 = new Client("ccc", 333333);
+
+            Order order1 = new Order(client1);
+            Order order2 = new Order(client2);
+            Order order3 = new Order(client3);
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+            order2.add(Goods.goods4);
+            order2.add(Goods.goods5);
+            order3.add(Goods.goods3, 2);
+            order3.add(Goods.goods4, 3);
+            order3.add(Goods.goods5);
+            OrderService.add_order(order1);
+            OrderService.add_order(order2);
+            OrderService.add_order(order3);
+            List<Order> ords = new List<Order>();
+            ords.Add(order1);
+            List<Goods> gds = new List<Goods>();
+            gds.Add(Goods.goods2);
+            gds.Add(Goods.goods3);
+            gds.Add(Goods.goods1);
+            CollectionAssert.Equals(OrderService.search_by_goods(gds), ords);
+        }
+        // search_by_sumPrice测试
+        public void TestMethod9()
+        {
+            Client client1 = new Client("aaa", 111111);
+            Client client2 = new Client("bbb", 222222);
+            Client client3 = new Client("ccc", 333333);
+
+            Order order1 = new Order(client1);
+            Order order2 = new Order(client2);
+            Order order3 = new Order(client3);
+
+            order1.add(Goods.goods2);
+            order1.add(Goods.goods1, 2);
+            order1.add(Goods.goods3);
+            order1.add(Goods.goods4);
+            order1.add(Goods.goods5);
+            order2.add(Goods.goods4);
+            order2.add(Goods.goods5);
+            order3.add(Goods.goods3, 2);
+            order3.add(Goods.goods4, 3);
+            order3.add(Goods.goods5);
+            OrderService.add_order(order1);
+            OrderService.add_order(order2);
+            OrderService.add_order(order3);
+            List<Order> ords = new List<Order>();
+            ords.Add(order1);
+            ords.Add(order3);
+
+            CollectionAssert.Equals(OrderService.search_by_sumPrice(27.0), ords);
+        }
     }
 }
